e8ade2afdaff04ffaac5267d45b86c1a56ed3426
Root <navaneeth.94@gmail.com>
2019-05-16 08:11:50 +0200

    Removed the junk files

--- NeuralNets/NNSYSID20/makemex.m
+++
@@ -1,7 +0,0 @@
-mex marq.c matrix.c nnmisc.c
-
-mex nnarmax2.c matrix.c nnmisc.c
-
-mex nnoe.c matrix.c nnmisc.c
-
-mex nnssif.c matrix.c nnmisc.c
--- NeuralNets/NNSYSID20/pmntanh.m
+++
@@ -1,7 +0,0 @@
-function t=pmntanh(x)
-% PMNTANH
-% -------
-% Fast hyperbolic tangent function to be used in
-% neural networks instead of the tanh provided by MATLAB
-t=1-2./(exp(2*x)+1);
-
